98c17183a8c0c6f2c3dfef63473b89367796c122
roma219 <roma219@yandex.ru>
2023-02-27 19:47:03 +0800

    dont send attachments without path

--- index.js
+++ index.js
@@ -38,35 +38,45 @@ class TeamCityReporter {
         break
       case 'failed':
       case 'timedOut':
+        console.log(result)
         const errorMessage = result.error.message
-        const errorDescription = result.errors.reduce(
-          (message, error) => `${message}\n\n${error.stack || ""}`,
-          ""
-        )
+        const errorDetails = result.error.stack || ''
 
         console.log(
-          `##teamcity[testFailed name='${testName}' message='${this.escape(
-            errorMessage,
-          )}' description='${this.escape(errorDescription)}' flowId='${this.flowId}']`,
+          `##teamcity[testFailed name='${testName}' message='${this.escape(errorMessage)}' details='${this.escape(
+            errorDetails,
+          )}' flowId='${this.flowId}']`,
         )
-        const failedStep = result.steps.find(step => step.category === 'test.step' && step.error)
+        const failedStep = result.steps.find((step) => step.category === 'test.step' && step.error)
+
         if (failedStep && failedStep.title) {
           let stepName = failedStep.title
+
           if (failedStep.steps.length) {
-            const subStep = failedStep.steps.find(step => step.category === 'test.step' && step.error)
+            const subStep = failedStep.steps.find((step) => step.category === 'test.step' && step.error)
+
             if (subStep) stepName = `${stepName} > ${subStep.title}`
           }
-          console.log(`##teamcity[testMetadata testName='${testName}' name='Failed at step' value='${stepName}']`)
+
+          console.log(
+            `##teamcity[testMetadata testName='${testName}' name='Failed at step' value='${stepName}' flowId='${this.flowId}']`,
+          )
         }
         if (this.artifactsFolder && result.attachments) {
-          result.attachments.forEach(attachment => {
-            let contentType = '';
+          result.attachments.forEach((attachment) => {
+            if (!attachment.path) return
+            let contentType = ''
+
             if (attachment.contentType.includes('video')) contentType = 'video'
             if (attachment.contentType.includes('image')) contentType = 'image'
             if (attachment.contentType.includes('application')) contentType = 'trace'
-            const metaDataName = contentType.charAt(0).toUpperCase() + contentType.slice(1);
+            const metaDataName = contentType.charAt(0).toUpperCase() + contentType.slice(1)
             const attachmentPath = `e2e/${attachment.path.split(this.artifactsFolder).pop()}`
-            console.log(`##teamcity[testMetadata testName='${testName}' name='${metaDataName}' type='${contentType === 'trace' ? 'Artifact' : contentType}' value='${attachmentPath}']`)
+            console.log(
+              `##teamcity[testMetadata testName='${testName}' name='${metaDataName}' type='${
+                contentType === 'trace' ? 'Artifact' : contentType
+              }' value='${attachmentPath}'] flowId='${this.flowId}'`,
+            )
           })
         }
         break
