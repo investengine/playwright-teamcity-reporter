cbe410573f3fd4c32f27525ad9cdfcc0ddc5dfd6
roma219 <roma219@yandex.ru>
2023-04-21 15:47:23 +0800

    log test results at the end for better teamcity handling

--- index.js
+++ index.js
@@ -16,81 +16,86 @@ class TeamCityReporter {
       .replace(/'/g, "|'")
   }
 
-  getTestName(test) {
-    return this.escape(`${test.parent.title} * ${test.title}`)
-  }
-
   onBegin(config, suite) {
     this.flowId = process.env.TEAMCITY_FLOWID || process.pid.toString()
-    console.log(`##teamcity[testRetrySupport enabled='true' flowId='${this.flowId}']`)
+    this.rootSuite = suite
   }
 
-  onTestBegin(test) {
-    const testName = this.getTestName(test)
-    console.log(`##teamcity[testStarted name='${testName}' flowId='${this.flowId}']`)
-  }
+  logTestResults(test) {
+    const testName = this.escape(test.title)
 
-  onTestEnd(test, result) {
-    const testName = this.getTestName(test)
-    switch (result.status) {
-      case 'skipped':
-        console.log(`##teamcity[testIgnored name='${testName}' flowId='${this.flowId}']`)
-        break
-      case 'failed':
-      case 'timedOut':
-        const errorMessage = result.error.message
-        const errorDescription = result.errors.reduce(
-          (message, error) => `${message}\n\n${error.stack || ""}`,
-          ""
-        )
+    test.results.forEach((result) => {
+      console.log(`##teamcity[testStarted name='${testName}' flowId='${this.flowId}']`)
 
-        console.log(
-          `##teamcity[testFailed name='${testName}' message='${this.escape(errorMessage)}' details='${this.escape(errorDescription)}' flowId='${this.flowId}']`,
-        )
-        const failedStep = result.steps.find((step) => step.category === 'test.step' && step.error)
-
-        if (failedStep && failedStep.title) {
-          let stepName = failedStep.title
-
-          if (failedStep.steps.length) {
-            const subStep = failedStep.steps.find((step) => step.category === 'test.step' && step.error)
-
-            if (subStep) stepName = `${stepName} > ${subStep.title}`
-          }
-
-          stepName = this.escape(stepName)
+      switch (result.status) {
+        case 'skipped':
+          console.log(`##teamcity[testIgnored name='${testName}' flowId='${this.flowId}']`)
+          break
+        case 'failed':
+        case 'timedOut':
+          const errorMessage =
+            result.status === 'failed' ? result.error.message : `Timeout of ${test.timeout}ms exceeded.`
+          const errorDescription = result.errors.reduce((message, error) => `${message}\n\n${error.stack || ''}`, '')
 
           console.log(
-            `##teamcity[testMetadata testName='${testName}' name='Failed at step' value='${stepName}' flowId='${this.flowId}']`,
+            `##teamcity[testFailed name='${testName}' message='${this.escape(errorMessage)}' details='${this.escape(
+              errorDescription,
+            )}' flowId='${this.flowId}']`,
           )
-        }
-        if (this.artifactsFolder && result.attachments) {
-          result.attachments.forEach((attachment) => {
-            if (!attachment.path) return
-            let contentType = ''
+          const failedStep = result.steps.find((step) => step.category === 'test.step' && step.error)
 
-            if (attachment.contentType.includes('video')) contentType = 'video'
-            if (attachment.contentType.includes('image')) contentType = 'image'
-            if (attachment.contentType.includes('application')) contentType = 'trace'
-            const metaDataName = contentType.charAt(0).toUpperCase() + contentType.slice(1)
-            const attachmentPath = `e2e/${attachment.path.split(this.artifactsFolder).pop()}`
+          if (failedStep && failedStep.title) {
+            let stepName = failedStep.title
+
+            if (failedStep.steps.length) {
+              const subStep = failedStep.steps.find((step) => step.category === 'test.step' && step.error)
+
+              if (subStep) stepName = `${stepName} > ${subStep.title}`
+            }
+
+            stepName = this.escape(stepName)
+
             console.log(
-              `##teamcity[testMetadata testName='${testName}' name='${metaDataName}' type='${
-                contentType === 'trace' ? 'Artifact' : contentType
-              }' value='${attachmentPath}'] flowId='${this.flowId}'`,
+              `##teamcity[testMetadata testName='${testName}' name='Failed at step' value='${stepName}' flowId='${this.flowId}']`,
             )
-          })
-        }
-        break
-      case 'passed':
-        break
-    }
-    console.log(
-      `##teamcity[testFinished name='${testName}' duration='${this.escape(result.duration)}' flowId='${this.flowId}']`,
-    )
+          }
+          if (this.artifactsFolder && result.attachments) {
+            result.attachments.forEach((attachment) => {
+              if (!attachment.path) return
+              let contentType = ''
+
+              if (attachment.contentType.includes('video')) contentType = 'video'
+              if (attachment.contentType.includes('image')) contentType = 'image'
+              if (attachment.contentType.includes('application')) contentType = 'trace'
+              const metaDataName = contentType.charAt(0).toUpperCase() + contentType.slice(1)
+              const attachmentPath = `e2e/${attachment.path.split(this.artifactsFolder).pop()}`
+              console.log(
+                `##teamcity[testMetadata testName='${testName}' name='${metaDataName}' type='${
+                  contentType === 'trace' ? 'Artifact' : contentType
+                }' value='${attachmentPath}'] flowId='${this.flowId}'`,
+              )
+            })
+          }
+          break
+        case 'passed':
+          break
+      }
+      console.log(
+        `##teamcity[testFinished name='${testName}' duration='${this.escape(result.duration)}' flowId='${
+          this.flowId
+        }']`,
+      )
+    })
+  }
+
+  onEnd() {
+    console.log(`##teamcity[testRetrySupport enabled='true'] flowId='${this.flowId}']`)
+
+    this.rootSuite.allTests().forEach((testCase) => {
+      this.logTestResults(testCase)
+    })
   }
 }
-
 
 const createReporter = (config = {}) => {
   return class CustomReporter extends TeamCityReporter {
@@ -101,5 +106,4 @@ const createReporter = (config = {}) => {
   }
 }
 
-
-module.exports.createReporter = createReporter;
+module.exports.createReporter = createReporter
